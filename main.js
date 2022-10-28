121cafa393dd2a3b618bff593cf8a62ae2a53b8e
Alexander <alexgriffith360@gmail.com>
2022-10-28 13:41:11 -0400

    create function to handle 4 arithetic operations

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const display = document.querySelector(".display");
 //
 //button refs
 const equal = document.querySelector(".equal");
+equal.addEventListener("click", calculate);
+
 const decimal = document.querySelector(".decimal");
 const clear = document.querySelector(".clear");
 const numbers = document.querySelectorAll(".number");
@@ -17,8 +19,51 @@ numbers.forEach((btn) =>
     handleNumber(e.target.textContent);
   })
 );
-
 function handleNumber(number) {
-  currNum += number;
-  display.textContent = currNum;
+  if (currNum.length <= 11) {
+    currNum += number;
+    display.textContent = currNum;
+  }
 }
+operators.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    handleOperator(e.target.textContent);
+  })
+);
+
+function handleOperator(op) {
+  operator = op;
+  prevNum = currNum;
+  currNum = "";
+  display.textContent = "";
+}
+
+function calculate() {
+  prevNum = Number(prevNum);
+  currNum = Number(currNum);
+  switch (operator) {
+    case "+":
+      prevNum += currNum;
+      break;
+    case "-":
+      prevNum -= currNum;
+      break;
+    case "x":
+      prevNum *= currNum;
+      break;
+    case "/":
+      if (currNum <= 0) {
+        prevNum = "Error can't divide by 0, dingus";
+        display.textContent = prevNum;
+        operator = "";
+        return;
+      }
+      prevNum /= currNum;
+      break;
+  }
+  prevNum.toString();
+  display.textContent = prevNum;
+  operator = "";
+}
+
+//resume from 21:50
